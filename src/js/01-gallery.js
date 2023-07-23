0718916a8ec4efe62c8b50935a0426f4fbc3fa08
JuliaChopyk <chopyk.julia@gmail.com>
2023-07-23 23:05:44 +0300

    second commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML(
+  'beforeend',
+  createImageCardsMarkup(galleryItems)
+);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, preview, original }) => {
+      return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'image') {
+    return;
+  }
+
+  const instance = SimpleLightbox.create(`
+  <div class="modal">
+<img
+      class="gallery__image"
+      src="${evt.target.dataset.source}" width = "800"
+      alt="${evt.target.alt}"
+    />
+    </div>
+`);
+
+  instance.show();
+}
